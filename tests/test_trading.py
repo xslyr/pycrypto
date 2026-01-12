64ab21bb6c6da016c39effdadf86ca7a39113447
xslyr <wesleysilv@gmail.com>
2026-01-12 12:18:56 -0300

    ADD: more tests for itemrule

--- tests/test_trading.py
+++ tests/test_trading.py
@@ -1,13 +1,24 @@
 import numpy as np
 import pytest
 
+from pycrypto.commons.database import Database
 from pycrypto.commons.utils import DataSources
 from pycrypto.trading import ItemRule, convert_data_to_numpy
 from pycrypto.trading.technical_analysis import Overlap
 from tests.broker_wrapper import BrokerWrapper
 
-data = BrokerWrapper().get_klines("BTCUSDT", "1h", "2025-01-01 00:00:00")
-data = convert_data_to_numpy(data, DataSources.mock)
+raw_data = BrokerWrapper().get_klines("BTCUSDT", "1h", "2025-01-01 00:00:00")
+data = convert_data_to_numpy(raw_data, DataSources.mock)
+
+
+def test_convert_mock_data_to_numpy():
+    assert not isinstance(raw_data, np.ndarray)
+    data = convert_data_to_numpy(raw_data, DataSources.mock)
+    assert isinstance(data, np.ndarray)
+
+
+def test_convert_db_data_to_numpy():
+    db_data = Database().select_klines("BTCUSD", "1d")
 
 
 @pytest.mark.parametrize(
@@ -55,6 +66,20 @@ def test_itemrule_must_return_active_field():
     assert arr_close._active_field == "close"
     assert isinstance(arr_close, ItemRule)
     assert all(np.asarray(ItemRule(data).active_field) == data["close"])
+
+
+def test_itemrule_must_return_last_line():
+    arr_close = ItemRule(data, "close")
+    assert isinstance(arr_close.last, ItemRule)
+    assert isinstance(arr_close.last[0], np.void)
+    assert np.asarray(arr_close.last) == data[-1:]
+    assert arr_close.last[0] == data[-1]
+
+
+def test_item_rule_must_allow_concatenate_properties_last_and_active_field():
+    arr_close = ItemRule(data, "close")
+    assert arr_close.active_field.last == data["close"][-1:]
+    assert arr_close.last.active_field == data["close"][-1:]
 
 
 def test_itemrule_must_return_equal_result_on_run_fnc_of_talib_func():
