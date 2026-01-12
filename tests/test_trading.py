e8f31b6e76b6c22f7faa2de753413e9c38919146
xslyr <wesleysilv@gmail.com>
2026-01-12 08:08:03 -0300

    UPD: enhancements on ItemRule; ADD: new tests for ItemRule

--- tests/test_trading.py
+++ tests/test_trading.py
@@ -1,7 +1,9 @@
+import numpy as np
 import pytest
 
 from pycrypto.commons.utils import DataSources
 from pycrypto.trading import ItemRule, convert_data_to_numpy
+from pycrypto.trading.technical_analysis import Overlap
 from tests.broker_wrapper import BrokerWrapper
 
 data = BrokerWrapper().get_klines("BTCUSDT", "1h", "2025-01-01 00:00:00")
@@ -33,4 +35,29 @@ data = convert_data_to_numpy(data, DataSources.mock)
 )
 def test_itemrule_must_be_comparable_with_a_number(key, operator, value, expected):
     arr = ItemRule(data, key)  # noqa: F841
-    assert eval(f"bool(arr {operator} {value})") == expected
+    assert eval(f"arr {operator} {value}") == expected
+
+
+def test_itemrule_must_be_comparable_one_each_other():
+    arr_close = ItemRule(data, "close")
+    arr_open = ItemRule(data, "open")
+    # open=96165.29  close=95469.28
+    assert not arr_close > arr_open
+    assert not arr_close >= arr_open
+    assert arr_close < arr_open
+    assert arr_close <= arr_open
+    assert not arr_close == arr_open
+    assert arr_close != arr_open
+
+
+def test_itemrule_must_return_active_field():
+    arr_close = ItemRule(data, "close")
+    assert arr_close._active_field == "close"
+    assert isinstance(arr_close, ItemRule)
+    assert all(np.asarray(ItemRule(data).active_field) == data["close"])
+
+
+def test_itemrule_must_return_equal_result_on_run_fnc_of_talib_func():
+    arr_close = ItemRule(data, "close", Overlap.sma, {"length": 5})
+    sma_result = Overlap.sma(data, length=5, target="close")
+    assert arr_close.run_fnc() == sma_result[-1]
