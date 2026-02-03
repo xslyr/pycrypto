463dbea2236e8f6b695b0ff189f3aac351d7d8d7
xslyr <wesleysilv@gmail.com>
2026-02-02 22:31:30 -0300

    UPD: `lint` & `github actions` adjusts

--- pycrypto/trading/analyst.py
+++ pycrypto/trading/analyst.py
@@ -1,15 +1,5 @@
 """Module responsable to implements Analyst class who will be used to decide when call buy/sell on broker"""
 
-"""
-  O analista(analyst) utiliza de uma estratégia de trading(TradeStrategy) ... 
-  que define em mercados(Market) as regras(ItemRule) para ações de compra/venda de ativos
-
-  As classes acima instanciam o cenário, ... 
-    o orchestrador executa a captura/disponibilização dos dados, salva em cache ou banco e...
-
-
-"""
-
 
 class Analyst:
     def __init__(self): ...
